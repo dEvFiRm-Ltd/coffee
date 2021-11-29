7af9eb3f52c6bd26397d1bf4f6d7d4e9a5417054
malinjr07 <malinjr07@gmail.com>
2021-11-29 12:04:21 +0600

    admin done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './Components/AdminPanel/Dashboard';
 import AdminPanel from './Components/AdminPanel/routes';
 import UserPanel from './Components/UserPanel';
 
 const App = () => {
-    return (<>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" exact element={<UserPanel />} />
-                <Route path="/admin" exact element={<AdminPanel />} />
-            </Routes>
-        </BrowserRouter>
-    </>
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" exact element={<UserPanel />} />
+                    <Route path="/admin" element={<AdminPanel />}>
+                        <Route path='dashboard' element={<Dashboard />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 };
 
--- frontend/src/Components/AdminPanel/Content/index.jsx
+++ frontend/src/Components/AdminPanel/Content/index.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Dashboard from '../Dashboard';
+import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar';
 import './main.css';
 
 const Content = () => {
     return (
         <div className="page-content pt-0">
+            <Outlet />
             <Sidebar />
-            <div className="content-wrapper">
-                <Routes>
-                    <Route path="/admin/dashboard" element={<Dashboard />} />
-                </Routes>
-            </div>
         </div>
     );
 };
