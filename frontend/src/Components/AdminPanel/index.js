1d8ea3c2b8ba873833ca5a42821ed604374e881c
malinjr07 <malinjr07@gmail.com>
2021-11-29 18:51:19 +0600

    admin file imported

--- frontend/src/Components/AdminPanel/index.js
+++ frontend/src/Components/AdminPanel/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import '../../vendor/css/all.css';
+import '../../vendor/css/icons/icomoon/styles.min.css';
 import Content from './Content';
 import Header from './Header';
 import Navbar from './Navbar';
