7eb2b8209c10efb46da1d090145f48af51f8d844
alemprogrammer <alemhhasib@gmail.com>
2021-11-29 15:54:39 +0600

    add upload middelware

--- backend/api/model/Contact.js
+++ backend/api/model/Contact.js
@@ -1,27 +1,31 @@
 const { Schema, model } = require('mongoose');
 
-const contactSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 3,
-        maxlength: 50,
-        trim: true,
+const contactSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            minlength: 3,
+            maxlength: 50,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        details: {
+            type: String,
+            required: true,
+            maxlength: 1500,
+        },
+        image: String,
     },
-    email: {
-        type: String,
-        required: true,
-    },
-    phone: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    details: {
-        type: String,
-        required: true,
-        minlength: 10,
-        maxlength: 1500,
-    },
-});
+    // eslint-disable-next-line prettier/prettier
+    { timestamps: true },
+);
 module.exports = model('Contact', contactSchema);
