29eab3d868f6008a1a4f6406538c1191fb363cbd
malinjr07 <malinjr07@gmail.com>
2022-03-16 15:43:45 +0600

    clean

--- backend/api/routers/authRouter.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { userSignupController, userLoingController } = require('../controllers/authController');
-
-router.post('/signup', userSignupController);
-router.post('/login', userLoingController);
-
-module.exports = router;
